a7bdcdf02fbdf0f63a4c5c78fa151ac0b824f22f
javithox <ja.quirogab@duocuc.cl>
2025-10-09 23:54:58 -0300

    commit4.1

--- vite-project/src/infoProducto/InfoProducto.tsx
+++ vite-project/src/infoProducto/InfoProducto.tsx
@@ -1,6 +1,8 @@
 import type { CSSProperties } from "react";
 import miLogo from '../logo-image/image-removebg-preview.png';
 
+
+
 const nombretienda:string='Level-Up'
 
 
@@ -19,11 +21,23 @@ export const InfoTienda = () => {
         <>
             <section className="contenedor-infoProducto">
                 <div>
-                    <h1 style={estiloTienda}>Productos</h1>
-                    <h2 style={estiloTienda}><img src={miLogo} alt="Logo" height="90px" width="90px" /> {nombretienda}</h2>
+                    <h1 style={estiloTienda}>Tienda Gamer </h1>
+                    <h2 style={estiloTienda}><img src={miLogo} alt="Logo" height="90px" width="90px" className="imagen-logo"/> {nombretienda}</h2>
+                    <nav>
+                        <a href="#" className="btn-nav">Inicio</a>
+                        <a href="#" className="btn-nav">Productos</a>
+                        <a href="#" className="btn-nav">Nosotros</a>
+                    </nav>
 
                 </div>
             </section>
+            <article>
+                <div>
+                    <h2 style={estiloTienda}>Sobre la tienda</h2>
+                    <img src="https://w0.peakpx.com/wallpaper/626/351/HD-wallpaper-level-up-gaming-cool-fun-game-level-up-next-level-play-playing-win-winner.jpg" alt="imagen level up" className="imagen-presentacion" />
+                    <p style={estiloTienda}>La tienda level up gamer tiene toda clase de productos para jugadores gamers de pc de toda gama</p>
+                </div>
+            </article>
                 
             
 
--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { InfoTienda } from './InfoProducto/InfoProducto'
+import { InfoTienda } from './InfoProducto/InfoProducto';
+import './estilo-Pagina.css';
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
     <InfoTienda/>
+    
   </StrictMode>,
 )
